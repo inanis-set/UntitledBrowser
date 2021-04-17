032366ae01ab89d69ab434be6ba60e32ca7a7530
NoTimeToExplain <artemiy.gvozdev.96@mail.ru>
2021-04-17 14:45:56 +0300

    Добавьте файлы проекта.

---
+++ UntitledBrowser/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace UntitledBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showDevToolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newTabToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeTabToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(44, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 27);
+            this.button2.TabIndex = 1;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(110, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(558, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(674, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 27);
+            this.button3.TabIndex = 3;
+            this.button3.Text = ">>";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(707, 10);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(27, 27);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(77, 10);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(27, 27);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "R";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showDevToolsToolStripMenuItem,
+            this.newTabToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(154, 48);
+            // 
+            // showDevToolsToolStripMenuItem
+            // 
+            this.showDevToolsToolStripMenuItem.Name = "showDevToolsToolStripMenuItem";
+            this.showDevToolsToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.showDevToolsToolStripMenuItem.Text = "Show DevTools";
+            this.showDevToolsToolStripMenuItem.Click += new System.EventHandler(this.showDevToolsToolStripMenuItem_Click);
+            // 
+            // newTabToolStripMenuItem
+            // 
+            this.newTabToolStripMenuItem.Name = "newTabToolStripMenuItem";
+            this.newTabToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.newTabToolStripMenuItem.Text = "New Tab";
+            this.newTabToolStripMenuItem.Click += new System.EventHandler(this.newTabToolStripMenuItem_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(721, 365);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Selecting += new System.Windows.Forms.TabControlCancelEventHandler(this.tabControl1_Selecting);
+            this.tabControl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.tabControl1_MouseDown);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(713, 339);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "New Tab";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.tabControl1);
+            this.panel1.Location = new System.Drawing.Point(13, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(721, 365);
+            this.panel1.TabIndex = 7;
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeTabToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(125, 26);
+            // 
+            // closeTabToolStripMenuItem
+            // 
+            this.closeTabToolStripMenuItem.Name = "closeTabToolStripMenuItem";
+            this.closeTabToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.closeTabToolStripMenuItem.Text = "Close Tab";
+            this.closeTabToolStripMenuItem.Click += new System.EventHandler(this.closeTabToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 421);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showDevToolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newTabToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem closeTabToolStripMenuItem;
+    }
+}
+
---
+++ UntitledBrowser/Form1.cs
@@ -0,0 +1,206 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace UntitledBrowser
+{
+    
+    public partial class Form1 : Form
+    {
+
+        private Point lastClickPos;
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+            textBox1.Text = "https://google.com";
+            ChromiumWebBrowser chrome = new ChromiumWebBrowser(textBox1.Text);
+            chrome.Parent = tabControl1.SelectedTab;
+            chrome.Dock = DockStyle.Fill;
+            chrome.AddressChanged += Chrome_AddressChanged;
+            chrome.TitleChanged += Chrome_TitleChanged;
+            tabControl1.TabPages.Insert(1, "   +");
+        }
+
+        private void Chrome_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                textBox1.Text = e.Address;
+            }));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            contextMenuStrip1.Show(button4, new Point(0, button4.Height));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser chrome = tabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            if (chrome != null)
+                chrome.Load(textBox1.Text);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser chrome = tabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            if (chrome != null)
+                chrome.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser chrome = tabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            if (chrome != null)
+            {
+                if (chrome.CanGoForward)
+                    chrome.Forward();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser chrome = tabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            if (chrome != null)
+            {
+                if (chrome.CanGoBack)
+                    chrome.Back();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void newTabToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TabPage tab = new TabPage();
+            tab.Text = "Google";
+            tabControl1.TabPages.Insert(tabControl1.TabCount - 1, tab);
+            tabControl1.SelectTab(tabControl1.TabCount - 2);
+            ChromiumWebBrowser chrome = new ChromiumWebBrowser("https://google.com");
+            chrome.Parent = tab;
+            chrome.Dock = DockStyle.Fill;
+            textBox1.Text = "https://google.com";
+            chrome.AddressChanged += Chrome_AddressChanged;
+            chrome.TitleChanged += Chrome_TitleChanged;
+        }
+
+        private void Chrome_TitleChanged(object sender, TitleChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                tabControl1.SelectedTab.Text = e.Title;
+            }));
+        }
+
+        private void showDevToolsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser chrome = tabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            chrome.ShowDevTools();
+        }
+
+        private void tabControl1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                lastClickPos = Cursor.Position;
+                this.contextMenuStrip2.Show(this.tabControl1, e.Location);
+            }
+        }
+
+        private void closeTabToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < tabControl1.TabCount - 1; i++)
+            {
+                if (tabControl1.GetTabRect(i).Contains(tabControl1.PointToClient(lastClickPos)))
+                {
+                    if (i == tabControl1.SelectedIndex)
+                    {
+                        tabControl1.TabPages[i].Dispose();
+                        try
+                        {
+                            tabControl1.SelectTab(i - 1);
+                        }
+                        catch (ArgumentOutOfRangeException)
+                        {
+                            tabControl1.SelectTab(0);
+                        }
+                    }
+                    else if (i != tabControl1.SelectedIndex)
+                    {
+                        tabControl1.TabPages[i].Dispose();
+                    }
+                }
+            }
+        }
+
+        private void tabControl1_Selecting(object sender, TabControlCancelEventArgs e)
+        {
+                if (e.TabPage.Text == "   +")
+                {
+                    TabPage tab = new TabPage();
+                    tab.Text = "Google";
+                    tabControl1.TabPages.Insert(tabControl1.TabCount - 1, tab);
+                    tabControl1.SelectTab(tabControl1.TabCount - 2);
+                    ChromiumWebBrowser chrome = new ChromiumWebBrowser("https://google.com");
+                    chrome.Parent = tab;
+                    chrome.Dock = DockStyle.Fill;
+                    textBox1.Text = "https://google.com";
+                    chrome.AddressChanged += Chrome_AddressChanged;
+                    chrome.TitleChanged += Chrome_TitleChanged;
+                }
+        }
+        // !!! all tabs have their own size and idk how to fix it !!!
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.T)
+            {
+                TabPage tab = new TabPage();
+                tab.Text = "Google";
+                tabControl1.TabPages.Insert(tabControl1.TabCount - 1, tab);
+                tabControl1.SelectTab(tabControl1.TabCount - 2);
+                ChromiumWebBrowser chrome = new ChromiumWebBrowser("https://google.com");
+                chrome.Parent = tab;
+                chrome.Dock = DockStyle.Fill;
+                textBox1.Text = "https://google.com";
+                chrome.AddressChanged += Chrome_AddressChanged;
+                chrome.TitleChanged += Chrome_TitleChanged;
+            }
+            if (e.Control && e.KeyCode == Keys.W)
+            {
+                //BUG:
+                //tab unselecting after action
+                int currentTab = tabControl1.SelectedIndex;
+                try
+                {
+                    tabControl1.SelectTab(currentTab - 1);
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    tabControl1.SelectTab(0);
+                }
+                tabControl1.TabPages[currentTab].Dispose();
+            }
+        }
+    }
+    //TODO (things i won't do, but you can do):
+    //text search (you can find it here: https://stackoverflow.com/questions/35884540/how-to-implement-text-search-in-cefsharp)
+    //downloading file (you can find it here: https://stackoverflow.com/questions/34289428/download-file-with-cefsharp-winforms)
+    //history and favorites (you can find it here: https://www.codeproject.com/Articles/60179/Web-Browser-in-C#History&Favorites4)
+    //zoom in/out (you can find it here: https://stackoverflow.com/questions/37681816/cefsharp-chromiumwebbrowser-allow-user-to-zoom-in-out)
+    //open new window (you can find it here: https://stackoverflow.com/questions/5957688/creating-windows-forms-open-new-form)
+    //i hope this helps someone
+}
